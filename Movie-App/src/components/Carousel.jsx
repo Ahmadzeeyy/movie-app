be84cf4277dbc2f24284f9853d6d06d536858278
Ahmads <bariziahnad19@gmail.com>
2025-05-27 21:51:33 +0700

    fix:carousel slider

--- Movie-App/src/components/Carousel.jsx
+++ Movie-App/src/components/Carousel.jsx
@@ -1,20 +1,30 @@
+import { useState } from "react";
 import { endpoints } from "../utils/endpoints";
 
 export default function Carousel({ trendingMovie }) {
+  const [slideIndex, setSlideIndex] = useState(1);
   return (
     <>
       <div className="carousel w-full ">
-        {trendingMovie.map((movie) => (
-          <div id="slide1" className="carousel-item relative w-full">
+        {trendingMovie.map((movie, index) => (
+          <div id={`slide${index}`} className="carousel-item relative w-full">
             <img
               src={`${endpoints.backdrop(movie.backdrop_path)}`}
               className="w-full h-[75vh] object-cover"
             />
             <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between bg-transparent ">
-              <a href="#slide4" className="btn btn-circle ">
+              <a
+                href={`#slide${
+                  (index - 1 + trendingMovie.length) % trendingMovie.length
+                }`}
+                className="btn btn-circle "
+              >
                 ❮
               </a>
-              <a href="#slide2" className="btn btn-circle  ">
+              <a
+                href={`#slide${(index + 1) % trendingMovie.length}`}
+                className="btn btn-circle  "
+              >
                 ❯
               </a>
             </div>
--- Movie-App/src/components/ListMovie.jsx
+++ Movie-App/src/components/ListMovie.jsx
@@ -3,7 +3,7 @@ import MovieItem from "./MovieITem";
 
 export default function ListMovie({ popularMovie, title }) {
   return (
-    <div classname="w-full">
+    <div className="w-full">
       <h1 className="w-full my-6 text-center text-2xl font-semibold">
         {title}
       </h1>
