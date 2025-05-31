91f4d67479ddfea669af2df7a5c7bc73261c74db
Ahmads <bariziahnad19@gmail.com>
2025-05-31 09:57:40 -0500

    add:detail pages

--- Movie-App/src/App.jsx
+++ Movie-App/src/App.jsx
@@ -3,9 +3,13 @@ import Carousel from "./components/carousel";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Hompage from "./Pages/Hompage";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 import { getTrendingMovie } from "./utils/movieAPI";
 import { useState } from "react";
+import DetailMovie from "./Pages/DetailMovie";
+
+
 function App() {
   const [trendingMovie, setTrendingMovie] = useState([]);
   useEffect(() => {
@@ -18,6 +22,15 @@ function App() {
   return (
     <>
       {/* <Navbar /> */}
+
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Hompage/>}/>
+          <Route path="/detail/:detailId" element={<DetailMovie/>}/>
+          <Route path="/detail/:detailId" element={<DetailMovie/>}/>
+
+      </Routes>
+      </BrowserRouter>
       <Carousel trendingMovie={trendingMovie} />
       <Hompage />
       <Footer />
